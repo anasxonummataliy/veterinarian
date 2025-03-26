22bf443ea976117461686c64ddc706860ea8e021
Anasxon Ummataliyev <aummataliy@gmail.com>
2025-03-27 01:13:26 +0500

    fix

--- backend/main.py
+++ backend/main.py
@@ -2,3 +2,15 @@ from fastapi import FastAPI
 
 
 app = FastAPI()
+
+data = [
+    {"name": "John", "age": 25},
+    {"name": "Jane", "age": 24},
+    {"name": "Jake", "age": 26},
+    {"name": "Jill", "age": 27}
+]
+
+
+@app.post("/")
+async def get_data():
+    return {"data": data}
