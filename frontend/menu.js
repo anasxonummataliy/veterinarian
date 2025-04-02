d771c311a92287b864e2ef80fcfc2fd34282e5d7
Anasxon Ummataliyev <aummataliy@gmail.com>
2025-04-02 23:54:36 +0500

    fix

--- frontend/menu.js
+++ frontend/menu.js
@@ -27,11 +27,37 @@ searchBtn.onclick = ()=>{
 
 
 // Video konteyner
+document.addEventListener("DOMContentLoaded", () => {
+  const video1 = document.getElementById("video1");
+  const playBtn1 = document.getElementById("play-btn1");
+  const overlay1 = document.querySelector(".overlay1");
+  const video2 = document.getElementById("video2");
+  const playBtn2 = document.getElementById("play-btn2");
+  const overlay2 = document.querySelector(".overlay2");
 
-document.getElementById("playButton").addEventListener("click", function () {
-  document.getElementById("video").play();
-  document.getElementById("overlay").style.opacity = "0";
-  setTimeout(() => {
-    document.getElementById("overlay").style.display = "none";
-  }, 500);
+  // Video 1
+  playBtn1.addEventListener("click", () => {
+    if (video1.paused) {
+      video1.play();
+      overlay1.style.opacity = "0";
+      playBtn1.style.opacity = "0";
+    } else {
+      video1.pause();
+      overlay1.style.opacity = "1";
+      playBtn1.style.opacity = "1";
+    }
+  });
+
+  // Video 2
+  playBtn2.addEventListener("click", () => {
+    if (video2.paused) {
+      video2.play();
+      overlay2.style.opacity = "0";
+      playBtn2.style.opacity = "0";
+    } else {
+      video2.pause();
+      overlay2.style.opacity = "1";
+      playBtn2.style.opacity = "1";
+    }
+  });
 });
