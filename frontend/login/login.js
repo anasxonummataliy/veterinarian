fad2523a351dd82197b2e9dde4622d7ef3e44a24
Anasxon Ummataliyev <aummataliy@gmail.com>
2025-04-25 22:54:02 +0500

    fix

--- frontend/login/login.js
+++ frontend/login/login.js
@@ -1,52 +1,66 @@
-const login = document.querySelector(".login-form");
+document.addEventListener("DOMContentLoaded", () => {
+    const login = document.querySelector(".login-form");
 
 
-login.addEventListener("submit", async (e) => {
-    e.preventDefault();
-    console.log("first")
+    login.addEventListener("submit", async (e) => {
+        e.preventDefault();
+        console.log("first")
 
-    const email = e.target.email.value
-    const password = e.target.password.value;
+        const email = e.target.email.value
+        const password = e.target.password.value;
 
 
-    if (!email || !password) {
-        alert("Email yoki password kiritilmadi!")
-        return;
-    }
-    if (!/\S+@\S+\.\S+/.test(email)) {
-        alert("Email xato kiritdingiz!")
-        return;
-    }
-    if (password.length < 6) {
-        alert("Password uzunligi kamida 6 ga teng bo'lishi kerak!");
-        return;
-    }
+        if (!email || !password) {
+            alert("Email yoki password kiritilmadi!")
+            return;
+        }
+        if (!/\S+@\S+\.\S+/.test(email)) {
+            alert("Email xato kiritdingiz!")
+            return;
+        }
+        if (password.length < 6) {
+            alert("Password uzunligi kamida 6 ga teng bo'lishi kerak!");
+            return;
+        }
 
-    try {
-        const login_response = await fetch("http://127.0.0.1:8000/auth/login/",
-            {
-                method: "POST",
-                headers:
+        try {
+            const login_response = await fetch("http://127.0.0.1:8000/auth/login/",
                 {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({
-                    email: email,
-                    password: password
-                })
-            }
-        );
-        const data = await login_response.json();
-        if (login_response.ok) {
-            console.log("Success", data);
-            document.cookie = `session=${JSON.stringify(data)}; SameSite=strict; Secure=true`;
-            window.location.href = "../user-page/index.html";
+                    method: "POST",
+                    headers:
+                    {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({
+                        email: email,
+                        password: password
+                    })
+                }
+            );
+            const data = await login_response.json();
+            if (login_response.ok) {
+                console.log("Success", data);
+                document.cookie = `session=${JSON.stringify(data)}; SameSite=strict; Secure=true`;
+                window.location.href = "../user-page/index.html";
 
+            }
+            else {
+
+            }
         }
-        else {
+        catch (error) {
+            console.log(`Eror : ${error}`)
         }
+    });
+
+    async function getUserMe() {
+        const token = localStorage.getItem("token")
+        if (!token) {
+            return null
+        }
+        const response = await fetch(`http://localhost:8000/auth/me/${token}`)
+        const data = await response.json()
+        return data
     }
-    catch (error) {
-        console.log(`Eror : ${error}`)
-    }
-});
+    getUserMe()
+})
